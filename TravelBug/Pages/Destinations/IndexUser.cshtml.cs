196f300202490ffa08ac5afe41515f53b1cc18a6
pratibhatiwarii <77544117+pratibhatiwarii@users.noreply.github.com>
2021-02-26 03:25:01 -0500

    help

---
+++ TravelBug/Pages/Destinations/IndexUser.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TravelBug.Data;
+using TravelBug.Models;
+
+namespace TravelBug.Pages.Destinations
+{
+    public class IndexUserModel : PageModel
+    {
+        private readonly TravelBug.Data.TravelBugContext _context;
+
+        public IndexUserModel(TravelBug.Data.TravelBugContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Destination> Destination { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Destination = await _context.Destination.ToListAsync();
+        }
+    }
+}
